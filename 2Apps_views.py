6b713e831e1ac0b398b8787fd9717657c22aec17
DhirendraBhatta <76986870+DhirendraBhatta@users.noreply.github.com>
2022-03-06 02:02:29 +0545

    Add files via upload

---
+++ 2Apps_views.py
@@ -0,0 +1,16 @@
+from turtle import ht
+from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("This is Homepage.")
+   
+def about(request):
+    return HttpResponse("This is About Page.")
+
+def services(request):
+    return HttpResponse("This is Services Page.")
+
+def contact(request):
+    return HttpResponse('This is Contact Page')
